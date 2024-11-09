90a543e1058055b6df17684c275d8c2f9642a8ba
Marcin Chodorek <marcin.chodorek3@gmail.com>
2024-11-09 19:40:48 +0100

    Add logs v2

--- app/api/firebase/serverConfig.server.ts
+++ app/api/firebase/serverConfig.server.ts
@@ -6,11 +6,16 @@ const serviceAccount = {
   privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 } as ServiceAccount;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+console.log(
+  "PRIVATE_KEY",
+  process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+);
+
+// if (!admin.apps.length) {
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+// }
 
 const db = admin.firestore();
 const auth = admin.auth();
