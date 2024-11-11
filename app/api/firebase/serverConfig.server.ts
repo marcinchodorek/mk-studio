fd9f81b8044277816587f4fcfdc3bbbb49f37080
Marcin Chodorek <marcin.chodorek3@gmail.com>
2024-11-11 22:11:07 +0100

    Change sdk config

--- app/api/firebase/serverConfig.server.ts
+++ app/api/firebase/serverConfig.server.ts
@@ -1,24 +1,15 @@
-import admin from "firebase-admin";
+import admin, { ServiceAccount } from "firebase-admin";
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+} as ServiceAccount;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      type: process.env.FIREBASE_ADMIN_PROJECT_TYPE,
-      project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_ADMIN_KEY_ID,
-      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
-        /\\n/g,
-        "\n",
-      ),
-      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
-      token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
-      auth_provider_x509_cert_url:
-        process.env.FIREBASE_ADMIN_AUTH_PROVIDER_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_PROVIDER_CERT_URL,
-      universe_domain: process.env.FIREBASE_ADMIN_UNIVERSE_DOMAIN,
-    } as never),
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${process.env.FIREBASE_ADMIN_PROJECT_ID}.firebaseio.com/`,
   });
 }
 
