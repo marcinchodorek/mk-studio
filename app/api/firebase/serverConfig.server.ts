e14104bb8f382d8b79ee473516240afa7cb4361b
Marcin Chodorek <marcin.chodorek3@gmail.com>
2024-11-14 20:52:21 +0100

    Revert "fix: tmp change cert for file"
    This reverts commit 024fe1a79dd3cbaea4b38d11d1fe9aabda1ab71e.

--- app/api/firebase/serverConfig.server.ts
+++ app/api/firebase/serverConfig.server.ts
@@ -1,14 +1,14 @@
 import admin, { ServiceAccount } from "firebase-admin";
 
-// const serviceAccount = {
-//   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-//   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-//   privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-// } as ServiceAccount;
+const serviceAccount = {
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+} as ServiceAccount;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert("app/mk-studio.json"),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: `https://${process.env.FIREBASE_ADMIN_PROJECT_ID}.firebaseio.com/`,
   });
 }
